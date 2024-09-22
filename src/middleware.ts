dc52c896784e916d16dc11e5a3467e719eb811ab
KrishKPs <kpskrishpatel@gmail.com>
2024-09-21 23:07:33 -0700

    Tried to Fix MiddleWare

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,23 @@
-import { NextResponse } from "next/server";
-import  { NextRequest } from "next/server";
-export { default } from "next-auth/middleware";
+import  { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
+import type { NextRequest } from "next/server"; 
 
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
 
-  if (token) {
-    if (
-      url.pathname === "/sign-in") {
-      return NextResponse.redirect(new URL("/dashboard", request.url));
-    }
-
-  } else {
-    // Redirect unauthenticated users away from protected pages
-    if (url.pathname.startsWith('/dashboard') || url.pathname.startsWith('/')) {
-      return NextResponse.redirect(new URL('/sign-in', request.url));
-    }
+  if (token && url.pathname === "/sign-in") {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
+  if (!token && (url.pathname.startsWith("/dashboard"))) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
 
-  return NextResponse.next()
+  return NextResponse.next();
 }
 
-// See "Matching Paths" 
+// See "Matching Paths"
 export const config = {
-  matcher: ["/sign-in", "/", '/dashboard'],
+  matcher: ["/sign-in", "/dashboard/"],
 };
